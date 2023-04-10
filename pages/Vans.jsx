4f812a80e3711dedbb7992563b3ec04bb3d34d54
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-10 20:42:21 +0300

    edit van cards to link to van detail page

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,10 +1,9 @@
-import React, {useState, useEffect} from "react"
-
+import React from "react"
+import { Link } from "react-router-dom"
 
 export default function Vans() {
-    const [vans, setVans] = useState([])
-    
-    useEffect(() => {
+    const [vans, setVans] = React.useState([])
+    React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
@@ -12,12 +11,14 @@ export default function Vans() {
 
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
     ))
 
