63d857f74108d8ab80e57a5d7b7cfab09151ddd7
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-12 16:46:18 +0300

    extract header

--- index.jsx
+++ index.jsx
@@ -11,13 +11,6 @@ import "./server"
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
