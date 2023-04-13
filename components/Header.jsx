e1de03859083f575ebffb8a9ff9cae9dd4ee7e9e
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-13 11:29:05 +0300

    add navlink and active class

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,29 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <Link to="/host">Host</Link>
-                <Link to="/about">About</Link>
-                <Link to="/vans">Vans</Link>
+                <NavLink 
+                    to="/host"
+                    className={({isActive}) => isActive ? "active-link" : null}
+                >
+                    Host
+                </NavLink>
+                <NavLink 
+                    to="/about"
+                    className={({isActive}) => isActive ? "active-link" : null}
+                >
+                    About
+                </NavLink>
+                <NavLink 
+                    to="/vans"
+                    className={({isActive}) => isActive ? "active-link" : null}
+                >
+                    Vans
+                </NavLink>
             </nav>
         </header>
     )
