54715fb05a45432473704eb7b38e691f58872641
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-13 11:04:45 +0300

    update vandetail component

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -16,9 +16,7 @@ export default function VanDetail() {
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
-                    <i className={`van-type ${van.type} selected`}>
-                        {van.type}
-                    </i>
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
                     <h2>{van.name}</h2>
                     <p className="van-price"><span>${van.price}</span>/day</p>
                     <p>{van.description}</p>
