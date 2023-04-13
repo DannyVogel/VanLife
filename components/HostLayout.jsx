d9dad8d0ddd9ad5f862a89759f01a4a6d646691b
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-13 18:49:04 +0300

    update paths to relative

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -12,7 +12,7 @@ export default function HostLayout() {
         <>
             <nav className="host-nav">
                 <NavLink
-                    to="/host"
+                    to="."
                     end
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
@@ -20,14 +20,21 @@ export default function HostLayout() {
                 </NavLink>
 
                 <NavLink
-                    to="/host/income"
+                    to="income"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Income
                 </NavLink>
+                
+                <NavLink
+                    to="vans"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
 
                 <NavLink
-                    to="/host/reviews"
+                    to="reviews"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Reviews
