86e8df77fb42ae3825934120ec5c8c824eddd19b
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-13 11:07:23 +0300

    add host link

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,6 +6,7 @@ export default function Header() {
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
+                <Link to="/host">Host</Link>
                 <Link to="/about">About</Link>
                 <Link to="/vans">Vans</Link>
             </nav>
