ece2023f6977edc6a5bfbfb5320ae78fb50f356f
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-13 18:48:35 +0300

    change styling method

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,25 +2,31 @@ import React from "react"
 import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
                 <NavLink 
                     to="/host"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
                 <NavLink 
                     to="/about"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({isActive}) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
                 <NavLink 
                     to="/vans"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
