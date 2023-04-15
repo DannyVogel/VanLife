8e90543ca276f1f524329b4bf39fa5b6a49762cb
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-15 12:11:12 +0300

    update server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { createServer, Model } from "miragejs"
+import { createServer, Model, Response } from "miragejs"
 
 
 createServer({
@@ -18,9 +18,11 @@ createServer({
     routes() {
         this.namespace = "api"
         this.logging = false
+        this.timing = 2000
 
         this.get("/vans", (schema, request) => {
-            return schema.vans.all()
+            return new Response(400, {}, {error: "Error fetching data"})
+            // return schema.vans.all()
         })
 
         this.get("/vans/:id", (schema, request) => {
