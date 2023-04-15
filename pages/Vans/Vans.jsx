fb041c4a39482e712f3599e7a62469071cf4fef9
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-15 10:20:40 +0300

    add link state

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -16,10 +16,13 @@ export default function Vans() {
     const displayedVans = typeFilter
         ? vans.filter(van => van.type === typeFilter)
         : vans
-    
+
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={van.id}>
+            <Link 
+                to={van.id} 
+                state={{ search: `?${searchParams.toString()}` }}
+            >
                 <img src={van.imageUrl} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
