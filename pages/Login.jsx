9d3c04c14c729aae0a439842bcc65405b51f1c93
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-15 17:52:38 +0300

    add loader for error message

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,8 +1,19 @@
 import React from "react"
-import { useNavigate } from "react-router-dom"
+import { useLoaderData } from "react-router-dom"
+
+/**
+ * Challenge: Pass a message from the requireAuth function
+ * that says "You must log in first." and display that message
+ * in an <h2> BELOW the <h1>. Give it a classname of "red" for
+ * some quick styling - (I added the CSS already).
+ */
+export function loader({ request }) {
+    return new URL(request.url).searchParams.get("message")
+}
 
 export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const message = useLoaderData()
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -20,6 +31,7 @@ export default function Login() {
     return (
         <div className="login-container">
             <h1>Sign in to your account</h1>
+            {message && <h3 className="red">{message}</h3>}
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
