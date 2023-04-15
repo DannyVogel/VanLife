4712f4fcdcc0380cef1d522502505dddcd11eb2c
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-15 17:52:17 +0300

    add error message

--- utils.js
+++ utils.js
@@ -4,6 +4,6 @@ export async function requireAuth() {
     const isLoggedIn = false
     
     if (!isLoggedIn) {
-        throw redirect("/login")
+        throw redirect("/login?message=You must log in first.")
     }
 }
