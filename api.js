62863dc793075788cc0a48df976f5be74a3ba105
DannyVogel <danny.hj.vogel@gmail.com>
2023-04-21 12:10:38 +0200

    refactor for using firestore

--- api.js
+++ api.js
@@ -1,3 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore/lite"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_k3v3HK3tKEqhlqFHPkwogW7PqEqhGhk",
+  authDomain: "vanlife-a1af5.firebaseapp.com",
+  projectId: "vanlife-a1af5",
+  storageBucket: "vanlife-a1af5.appspot.com",
+  messagingSenderId: "803007000356",
+  appId: "1:803007000356:web:446cd3a1ca406839258db1"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, "vans")
+
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
